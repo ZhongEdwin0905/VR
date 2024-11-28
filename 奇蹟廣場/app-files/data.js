68b73b432cbb02982dd80cfa70c46c4efb2e6655
ZhongEdwin0905 <a0958817969@gmail.com>
2024-11-28 11:32:08 +0800

    奇蹟廣場

---
+++ 奇蹟廣場/app-files/data.js
@@ -0,0 +1,238 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "比薩斜塔",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.9185369148331226,
+        "pitch": -0.1590907817520062,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0883388261892906,
+          "pitch": 0.3101016681061992,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.8199726862510843,
+          "pitch": 0.369902227500404,
+          "rotation": 0.7853981633974483,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.091275099681657,
+          "pitch": -0.718207791021273,
+          "title": "比薩斜塔",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "比薩斜塔後面",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.5957459672734977,
+        "pitch": -0.17668598226512344,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1330073760556,
+          "pitch": 0.21096233945030107,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "比薩斜塔側面-右前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8016534506954471,
+        "pitch": -0.32405464748365276,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0153181556248416,
+          "pitch": 0.4981545733284829,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.1292120950295246,
+          "pitch": 0.310358310183414,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "比薩斜塔左側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.12186715344153143,
+        "pitch": -0.2404810273572089,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1757078863701338,
+          "pitch": 0.1656409627841402,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.8173655615671827,
+          "pitch": 0.5706272193384372,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "比薩斜塔正面",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.959555810535462,
+        "pitch": -0.22995455701132883,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8332300310247938,
+          "pitch": 0.24110208125266652,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.5208879904936135,
+          "pitch": 0.457633750383728,
+          "rotation": 5.497787143782138,
+          "target": "2--"
+        },
+        {
+          "yaw": -2.4175377138892316,
+          "pitch": 0.41094704986091557,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "比薩斜塔",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
