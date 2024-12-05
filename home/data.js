fa3d6868954d5e1de2f037f719544a890ac0f6f9
ZhongEdwin0905 <a0958817969@gmail.com>
2024-12-05 10:39:45 +0800

    home

---
+++ home/data.js
@@ -0,0 +1,174 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "比薩主教座堂外觀",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.900666507729923,
+        "pitch": -0.10340112988203565,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.796384597032901,
+          "pitch": 0.07398441134225564,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0638970315084624,
+          "pitch": -0.29219303723489176,
+          "title": "比薩主教座堂",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "比薩主教座堂內部-後",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.923851978408681,
+        "pitch": -0.038417410841580235,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.098142413819991,
+          "pitch": 0.31443093666295496,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.19701827044013953,
+          "pitch": 0.45992448324311397,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "比薩主教座堂內部-前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.4528545018261916,
+        "pitch": 0.06448170555006172,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7662608321817572,
+          "pitch": 0.16754527858502577,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -1.6294486894587195,
+          "pitch": 0.2349667982348329,
+          "rotation": 0.7853981633974483,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "比薩主教座堂內部-中",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.8450623521082967,
+        "pitch": 0.025161115162710246,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6354559074096828,
+          "pitch": 0.3478559548990603,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "比薩主教座堂",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
